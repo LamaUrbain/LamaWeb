fce0713bbee3a9cb42e3fcb4b77d364ad11d9137
Enguerrand Decorne <decorne.en@gmail.com>
2015-09-09 13:29:34 -0700

    Delete an itinerary from the list of itineraries

--- lamaweb/static/js/itineraries.js
+++ lamaweb/static/js/itineraries.js
@@ -1,4 +1,24 @@
 
-// TODO
-// handle delete form on click on delete button
-// on success, delete the table row that corresponds to the deleted itinerary
+$(document).ready(function() {
+    // handle delete form on click on delete button
+    $('button[name=deleteItinerary]').click(function(e) {
+	e.preventDefault();
+	var button = $(this);
+	var form = button.closest('form');
+	var itinerary_id = form.find('input[name=itinerary]').val();
+	$.ajax({
+	    type: 'POST',
+	    url: '/ajax/form/deleteitinerary',
+	    data: {
+		'itinerary': itinerary_id,
+	    },
+	    success: function(data) {
+		// on success, delete the table row that corresponds to the deleted itinerary
+		button.closest('tr').prev().remove();
+		button.closest('tr').remove();
+	    },
+	    error: genericAjaxError,
+	});
+	return false;
+    });
+});
